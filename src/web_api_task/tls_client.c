450c6a65daeeb5a6d65e485fde2abf95d1e9ada7
Tep1g <66882646+Tep1g@users.noreply.github.com>
2025-05-04 19:10:08 -0500

    rename wifi module to tls_client

--- src/web_api_task/tls_client.c
+++ src/web_api_task/tls_client.c
@@ -1,20 +1,20 @@
-#include "wifi.h"
+#include "tls_client.h"
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
 static char ssid[];
 static char password[];
 
-enum wifi_return_status wifi_init_and_connect() {
+enum tls_return_status tls_client_init() {
     if(cyw43_arch_init_with_country(CYW43_COUNTRY_USA)) {
-        return WIFI_STATUS_ERROR;
+        return TLS_STATUS_ERROR;
     }
 
     cyw43_arch_enable_sta_mode();
 
     if(cyw43_arch_wifi_connect_timeout_ms(ssid, password, CYW43_AUTH_WPA2_AES_PSK, 10000)) {
-        return WIFI_STATUS_ERROR;
+        return TLS_STATUS_ERROR;
     }
 
-    return WIFI_STATUS_OK;
+    return TLS_STATUS_ERROR;
 }
