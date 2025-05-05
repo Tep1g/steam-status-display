97a6a85350555ec89c301f5d0111491b5e67b528
Tep1g <66882646+Tep1g@users.noreply.github.com>
2025-05-04 20:09:25 -0500

    remove tls_return_status enum

--- src/web_api_task/tls_client.c
+++ src/web_api_task/tls_client.c
@@ -5,16 +5,10 @@
 static char ssid[];
 static char password[];
 
-enum tls_return_status tls_client_init() {
-    if(cyw43_arch_init_with_country(CYW43_COUNTRY_USA)) {
-        return TLS_STATUS_ERROR;
-    }
+void tls_client_init() {
+    if(cyw43_arch_init_with_country(CYW43_COUNTRY_USA));
 
     cyw43_arch_enable_sta_mode();
 
-    if(cyw43_arch_wifi_connect_timeout_ms(ssid, password, CYW43_AUTH_WPA2_AES_PSK, 10000)) {
-        return TLS_STATUS_ERROR;
-    }
-
-    return TLS_STATUS_ERROR;
+    while(cyw43_arch_wifi_connect_timeout_ms(ssid, password, CYW43_AUTH_WPA2_AES_PSK, 4000));
 }
--- src/web_api_task/tls_client.h
+++ src/web_api_task/tls_client.h
@@ -1,11 +1,6 @@
 #ifndef TLS_CLIENT_H
 #define TLS_CLIENT_H
 
-enum tls_return_status {
-    TLS_STATUS_OK = 0,
-    TLS_STATUS_ERROR = -1,
-};
-
-enum tls_return_status tls_client_init();
+void tls_client_init();
 
 #endif /* TLS_CLIENT_H */
