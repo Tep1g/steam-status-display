154b929e0986c5ad0c00dc9d286891839d610496
Tep1g <66882646+Tep1g@users.noreply.github.com>
2025-05-17 16:35:46 -0500

    add and init interthread steam_user_data_t struct

--- src/web_api_task/https_client.c
+++ src/web_api_task/https_client.c
@@ -4,6 +4,7 @@
 #include "pico/cyw43_arch.h"
 
 #define PLAYER_SUMMARIES_MAX_SIZE 3000
+#define PLAYER_NAME_MAX_SIZE 33
 #define PLAYER_ICON_MAX_SIZE 8000
 #define PLAYER_GAME_ICON_MAX_SIZE 4000 
 
@@ -12,6 +13,12 @@ static char password[];
 
 static bool https_client_inited = false;
 
+static struct steam_user_data_t steam_user_data = {
+    .data_is_ready = false
+};
+
+struct steam_user_data_t *get_steam_user_data_ptr() {
+    return &steam_user_data;
 void https_client_init() {
     if (https_client_inited) {
         return;
@@ -24,6 +31,10 @@ void https_client_init() {
     while(cyw43_arch_wifi_connect_timeout_ms(ssid, password, CYW43_AUTH_WPA2_AES_PSK, 4000));
 
     init_steam_request_data(PLAYER_SUMMARIES_MAX_SIZE, PLAYER_ICON_MAX_SIZE, PLAYER_GAME_ICON_MAX_SIZE);
+
+    steam_user_data.display_name = malloc(PLAYER_NAME_MAX_SIZE*sizeof(char));
+    steam_user_data.avatar_icon_jpg = malloc(PLAYER_ICON_MAX_SIZE*sizeof(uint8_t));
+    steam_user_data.game_icon_jpg = malloc(PLAYER_GAME_ICON_MAX_SIZE*sizeof(uint8_t));
 
     https_client_inited = true;
 }
--- src/web_api_task/https_client.h
+++ src/web_api_task/https_client.h
@@ -1,6 +1,19 @@
 #ifndef TLS_CLIENT_H
 #define TLS_CLIENT_H
 
+#include <stdbool.h>
+#include <semphr.h>
+
+struct steam_user_data_t {
+    bool data_is_ready;
+    SemaphoreHandle_t mutex;
+    char *display_name;
+    uint8_t *avatar_icon_jpg;
+    uint8_t *game_icon_jpg;
+};
+
+struct steam_user_data_t *get_steam_user_data_ptr(void);
+
 void https_client_init();
 
 #endif /* HTTPS_CLIENT_H */
