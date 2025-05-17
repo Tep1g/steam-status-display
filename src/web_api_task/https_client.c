5cf01e6eb23823071a5c72c228a11ef0809cf50a
Tep1g <66882646+Tep1g@users.noreply.github.com>
2025-05-17 14:52:29 -0500

    add https_client_inited boolean guard

--- src/web_api_task/https_client.c
+++ src/web_api_task/https_client.c
@@ -5,10 +5,18 @@
 static char ssid[];
 static char password[];
 
+static bool https_client_inited = false;
+
 void https_client_init() {
+    if (https_client_inited) {
+        return;
+    }
+
     if(cyw43_arch_init_with_country(CYW43_COUNTRY_USA));
 
     cyw43_arch_enable_sta_mode();
 
     while(cyw43_arch_wifi_connect_timeout_ms(ssid, password, CYW43_AUTH_WPA2_AES_PSK, 4000));
+
+    https_client_inited = true;
 }
