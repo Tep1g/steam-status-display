14cceb6ef484365c72625325e8b3837842761697
Tep1g <66882646+Tep1g@users.noreply.github.com>
2025-05-22 20:02:08 -0500

    add web api task to main and bind it to core 1

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 #include "lcd.h"
 #include "https_client.h"
 
+#define WEB_API_TASK_STACK_SIZE 4096U
+
 void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName ) {
     ( void ) pxTask;
     ( void ) pcTaskName;
@@ -26,6 +28,12 @@ void main() {
     xTaskCreate(lcd_task, "LCD Task", 2048, NULL, 1, &lcd_task_handle);
     UBaseType_t lcd_task_uxCoreAffinityMask = 1 << 0; // Set core affinity to core 0
     vTaskCoreAffinitySet(lcd_task_handle, lcd_task_uxCoreAffinityMask);
+
+    TaskHandle_t web_api_task_handle;
+    xTaskCreate(https_client_task, "Web API Task", WEB_API_TASK_STACK_SIZE, NULL, 1, &web_api_task_handle);
+    UBaseType_t web_api_task_uxCoreAffinityMask = 1 << 1; // Set core affinity to core 1
+    vTaskCoreAffinitySet(web_api_task_handle, web_api_task_uxCoreAffinityMask);
+
     vTaskStartScheduler();
 
     return 0;
