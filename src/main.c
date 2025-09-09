479415b9aa3343df10847655a4656e7151480b54
Tep1g <66882646+Tep1g@users.noreply.github.com>
2025-09-09 14:37:34 -0500

    verify toolchain
    successfully compile, and upload blinking light program to pico pico

--- src/main.c
+++ src/main.c
@@ -1,29 +1,22 @@
 #include "pico/stdlib.h"
-#include "FreeRTOS.h"
-#include "task.h"
-#include "lcd.h"
-#include "https_client.h"
-
-#define WEB_API_TASK_STACK_SIZE 2048U
-
-#define LCD_TASK_STACK_SIZE 2048
+// #include "FreeRTOS.h"
+// #include "task.h"
+// #include "lcd.h"
+// #include "https_client.h"
 
 int main() {
     stdio_init_all();
 
-    (get_steam_user_data_ptr())->mutex = xSemaphoreCreateMutex();
-    
-    TaskHandle_t lcd_task_handle;
-    xTaskCreate(lcd_task, "LCD Task", LCD_TASK_STACK_SIZE, NULL, 1, &lcd_task_handle);
-    UBaseType_t lcd_task_uxCoreAffinityMask = 1 << 0; // Set core affinity to core 0
-    vTaskCoreAffinitySet(lcd_task_handle, lcd_task_uxCoreAffinityMask);
+    gpio_init(16);
+    gpio_set_dir(16, GPIO_OUT);
 
-    TaskHandle_t web_api_task_handle;
-    xTaskCreate(https_client_task, "Web API Task", WEB_API_TASK_STACK_SIZE, NULL, 1, &web_api_task_handle);
-    UBaseType_t web_api_task_uxCoreAffinityMask = 1 << 1; // Set core affinity to core 1
-    vTaskCoreAffinitySet(web_api_task_handle, web_api_task_uxCoreAffinityMask);
+    while(1) {
+        sleep_ms(1000);
+        gpio_put(16, 1);
 
-    vTaskStartScheduler();
+        sleep_ms(1000);
+        gpio_put(16, 0);
+    }
 
     return 0;
 }
